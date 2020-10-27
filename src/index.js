8e8d6f3b54496ab106c1656a6f3eb6ba72163f21
Tom <th.kobierecki@gmail.com>
2020-10-27 16:11:13 +0000

    Update index.js
    update index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import Firebase, { FirebaseContext } from './Firebase';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Firebase, { FirebaseContext } from "./Firebase";
+//here are the changes froom 4 commit
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
     <App />
   </FirebaseContext.Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
